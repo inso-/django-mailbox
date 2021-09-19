d1bd03f4167a5927babeb6f31b06eadc496363ab
Thomas Moussajee <thomas.moussajee@gmail.com>
2021-09-19 21:26:09 +0100

    add smt_uri migration and add use_gmail property

--- django_mailbox/models.py
+++ django_mailbox/models.py
@@ -189,6 +189,11 @@ class Mailbox(models.Model):
         return '+ssl' in self._protocol_info.scheme.lower()
 
     @property
+    def use_gmail(self):
+        """Returns whether or not this mailbox's connection uses GMAIL."""
+        return 'gmail+ssl' in self._protocol_info.scheme.lower()
+
+    @property
     def use_tls(self):
         """Returns whether or not this mailbox's connection uses STARTTLS."""
         return '+tls' in self._protocol_info.scheme.lower()
